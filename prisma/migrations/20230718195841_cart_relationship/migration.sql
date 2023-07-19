9a898a20e8a593c5bd5420bf5c1322de939ca2fc
cdirks4 <cdirks4@me.com>
2023-07-19 12:50:05 -0400

    fix(db): correct migrations

--- prisma/migrations/20230718195841_cart_relationship/migration.sql
+++ prisma/migrations/20230718195841_cart_relationship/migration.sql
@@ -3,10 +3,11 @@
 
   - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[owner_id]` on the table `cart` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `owner_id` to the `cart` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "cart" ADD COLUMN     "owner_id" UUID;
+ALTER TABLE "cart" ADD COLUMN     "owner_id" UUID NOT NULL;
 
 -- DropTable
 DROP TABLE "User";
@@ -26,4 +27,4 @@ CREATE UNIQUE INDEX "user_externalId_key" ON "user"("externalId");
 CREATE UNIQUE INDEX "cart_owner_id_key" ON "cart"("owner_id");
 
 -- AddForeignKey
-ALTER TABLE "cart" ADD CONSTRAINT "cart_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "cart" ADD CONSTRAINT "cart_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
