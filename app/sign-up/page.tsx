52718fbf8cb6056b4a50c7ea5a10b4301b32e02a
cdirks4 <cdirks4@me.com>
2023-07-27 10:10:42 -0400

    feat: add instructions surrounding signup

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
     return (
-        <div className="flex justify-center">
-            <SignUp />
+        <div className="max-w-xl mx-auto ">
+            <h1 className="text-center text-md font-bold ">
+                To avoid unneccesarry friction I have removed email verification
+            </h1>
+            <p className="text-center text-sm mb-2">
+                The only requirements are it must include an @ and a domain.
+            </p>
+            <div className="flex justify-center">
+                <SignUp />
+            </div>
         </div>
     );
 }
